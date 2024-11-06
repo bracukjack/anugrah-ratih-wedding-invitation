597df66c58cc1c04e34d94b646b01dd90242326e
widi <gede.widyadnyana@taksu.tech>
2024-11-06 20:35:50 +0800

    fix typo

--- src/app/invitation/page.tsx
+++ src/app/invitation/page.tsx
@@ -174,7 +174,7 @@ const InvitationPage = () => {
 
             <div>
               <Image
-                src="/svg/brige.svg"
+                src="/svg/bride.svg"
                 className="w-full"
                 alt="bridge"
                 width={1000}
